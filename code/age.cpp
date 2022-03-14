ee0353ca7b15df8286d58b225103d7c03c4f2cd1
MarkGotLasagna <marcorondelli1@gmail.com>
2022-03-14 07:17:26 +0100

    formatting

---
+++ code/age.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    
+    int age,year,yearf;
+    int res;
+
+    cout << "Insert the current year, your age, and the year in the future: ";
+    cin >> age >> year >> yearf;
+
+
+
+}
