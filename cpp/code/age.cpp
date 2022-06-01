0f0bf0358220f39935d1832700ad714f2b9e80a0
MarkGotLasagna <marcorondelli1@gmail.com>
2022-06-01 20:38:36 +0200

    added text file

--- cpp/code/age.cpp
+++ cpp/code/age.cpp
@@ -1,17 +1,38 @@
+/*  PROBLEM
+    write a program that takes as inputs the current year, your age,
+    and a year in the future/past, to calculate what your age will be/was
+*/
+
 #include <iostream>
 
-using std::cin;
-using std::cout;
-using std::endl;
+using namespace std;
 
 int main() {
-    
-    int age,year,yearf;
-    int res;
 
-    cout << "Insert the current year, your age, and the year in the future: ";
-    cin >> age >> year >> yearf;
-
-
-
+	int x, y, z, k, h;
+	cout << "Input the current year: ";
+	cin >> x;
+	cout << "Input your age: ";
+	cin >> y;
+	z = x-y;
+	cout << "Input the year in the future: ";
+	cin >> k;
+		
+	if(k>z && k<x) {
+		h= y+(k-x);
+		cout << "Your age in "<< k <<" was "<< h << endl;
+	}
+	else if(k>x){
+		h= y+(k-x);
+		cout << "Your age in "<< k <<" will be "<< h <<endl;
+	}
+	
+	else cout << "In the year "<< k <<" you still weren't born " << endl;		
+	return 0;		
 }
+
+/*	SOLUTION
+	store in some variables (say x,y,k) the values required for the calculations,
+	the age in the past/future can be calculated as follows: h=age+(futurepast-present)
+	add some checks: if present-futurepast is a negative number
+*/
