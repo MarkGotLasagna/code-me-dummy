bb1ca9ece36334403144c08a2f977b85af2d8fb6
MarkGotLasagna <marcorondelli1@gmail.com>
2022-06-01 17:23:28 +0200

    inglese cristo

--- cpp/code/età.cpp
+++ cpp/code/età.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
+
 using namespace std;
-int main()
-{
-int x,y,z,k,h;
-	cout<<"Inserisci l'anno corrente";
-	cin>>x; // anno corrente
-	cout<<"inserisci la tua eta'";
-	cin>>y; 					 //eta' attuale
-	z=x-y;						 //anno di nascita
-	cout<<"Inserisci l'anno per calcolare la tua eta'";
-	cin>>k; //anno inserito
+
+int main() {
+
+	int x, y, z, k, h;
+	cout << "Input the current year: ";
+	cin >> x;
+	cout << "Input your age: ";
+	cin >> y; 					 //eta' attuale
+	z = x-y;						 //anno di nascita
+	cout << "Input the year in the future: ";
+	cin >> k;
 		
-	if(k>z && k<x){
-			h= y+(k-x);
-		cout<<"La tua eta' nel "<<k<<" era di "<<h<<" anno/i "<<endl;
+	if(k>z && k<x) {
+		h= y+(k-x);
+		cout << "Your age in "<< k <<" was "<< h << endl;
+	}
+	else if(k>x){
+		h= y+(k-x);
+		cout << "Your age in "<< k <<" will be "<< h <<endl;
 	}
 	
-		
-		else if(k>x){
-				  h= y+(k-x);
-		  cout<<"La tua eta' nel "<<k<<" sara' di "<<h<<" anni "<<endl;
-		}
-	
-		
-			else 
-			cout<<"Nel "<<k<<" non eri ancora nato "<<endl;
-			
-		
+	else cout << "In the year "<< k <<" you still weren't born " << endl;		
 	return 0;		
-	
-	
-
-	
 }
