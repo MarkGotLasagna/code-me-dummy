1eac35897995ee18625dedfd1d24c31525ea548d
MarkGotLasagna <marcorondelli1@gmail.com>
2022-06-02 12:19:07 +0200

    added python files

---
+++ python/code/helloworld.py
@@ -0,0 +1,8 @@
+#   PROBLEM
+#   try writing a simple script to print out "Hello world!"
+
+print("Hello, world!")
+
+#   SOLUTION
+# print() is part of the standard library of python
+# it's a built-in function to print to screen some lines, values and so on
